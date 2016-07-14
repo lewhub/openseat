4dd0accd02205436c2a29995c99f3a7883fc92cf
lewhub <bracey.lewis@gmail.com>
2016-07-13 19:41:21 -0500

    added web view logic for linux, windows, and mac machines

--- www/js/app.js
+++ www/js/app.js
@@ -2,15 +2,6 @@ angular.module('library_app', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-     var isWebView;
-     if (ionic.Platform.platform() === "macintel" || ionic.Platform.platform() === "win32" || ionic.Platform.platform() === "linux"){
-       isWebView = true;
-     } else {
-       isWebView = false;
-     }
-     console.log(ionic.Platform.platform())
-      console.log("visiting on web>>>", isWebView)
-      console.log("hello world")
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
--- www/js/config.js
+++ www/js/config.js
@@ -1,38 +1,60 @@
 (function(){
     angular.module("library_app")
         .config(function($stateProvider, $urlRouterProvider, $httpProvider){
-            $httpProvider.interceptors.push("authInterceptor");
-            $urlRouterProvider.otherwise("/signup");
-            
-            $stateProvider
-                .state("signup", {
-                    url: "/signup",
-                    templateUrl: "partials/signup.html",
-                    controller: "SignUpCtrl as signup_ctrl"
-                })
-                .state("confirm-email", {
-                    url: "/confirm",
-                    templateUrl: "partials/confirm_email.html"
-                })
-                .state("login", {
-                    url: "/login",
-                    templateUrl: "partials/login.html",
-                    controller: "LoginCtrl as login_ctrl"
-                })
-                .state("library_list", {
-                    url: "/library-list",
-                    templateUrl: "partials/library_list.html",
-                    controller: "LibraryListCtrl as library_list_ctrl"
-                })
-                .state("library_feed", {
-                    url: "/library-feed",
-                    templateUrl: "partials/library_feed.html",
-                    controller: "LibraryFeedCtrl as library_feed_ctrl"
-                })
-                .state("floor_blueprint", {
-                    url: "/floor-blueprint",
-                    templateUrl: "partials/floor_blueprint.html"
-                })
+            var isWebView = function(){
+                if (ionic.Platform.platform() === "macintel" || ionic.Platform.platform() === "win32" || ionic.Platform.platform() === "linux"){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            if (!isWebView()){
+                $httpProvider.interceptors.push("authInterceptor");
+                $urlRouterProvider.otherwise("/signup");
+                
+                $stateProvider
+                    .state("signup", {
+                        url: "/signup",
+                        templateUrl: "partials/signup.html",
+                        controller: "SignUpCtrl as signup_ctrl"
+                    })
+                    .state("confirm-email", {
+                        url: "/confirm",
+                        templateUrl: "partials/confirm_email.html"
+                    })
+                    .state("login", {
+                        url: "/login",
+                        templateUrl: "partials/login.html",
+                        controller: "LoginCtrl as login_ctrl"
+                    })
+                    .state("library_list", {
+                        url: "/library-list",
+                        templateUrl: "partials/library_list.html",
+                        controller: "LibraryListCtrl as library_list_ctrl"
+                    })
+                    .state("library_feed", {
+                        url: "/library-feed",
+                        templateUrl: "partials/library_feed.html",
+                        controller: "LibraryFeedCtrl as library_feed_ctrl"
+                    })
+                    .state("floor_blueprint", {
+                        url: "/floor-blueprint",
+                        templateUrl: "partials/floor_blueprint.html"
+                    })
+            } else {
+                $urlRouterProvider.otherwise("/");
+
+                $stateProvider
+                    .state("home", {
+                        url: "/",
+                        templateUrl: "partials/home.html"
+                    })
+                    .state("about", {
+                        url: "/about",
+                        templateUrl: "partials/about.html"
+                    })
+            }
+          
          
 
         })
