68f362e4f7d0a565eaba029657356be43ddea35c
lewhub <bracey.lewis@gmail.com>
2016-07-16 17:58:01 -0500

    attempting to see response 2

--- www/js/controllers/confirm_email_ctrl.js
+++ www/js/controllers/confirm_email_ctrl.js
@@ -11,7 +11,7 @@
             var errCallback = function(res){
                 console.log("error >>>", res);
                 var pTag = angular.element( document.querySelector("#p-tag-test") )
-                    pTag.text(res);
+                    pTag.text("Failure to show user");
                      $scope.$broadcast("scroll.refreshComplete");
             }
 
@@ -24,7 +24,7 @@
                     user_fac.grant_token(res.data.user.email).then(function(res){
                         console.log(res);
                          var pTag = angular.element( document.querySelector("#p-tag-test") )
-                    pTag.text("Success!!!");
+                         pTag.text("Success!!!");
                         $state.go("library_list")
                         $scope.$broadcast("scroll.refreshComplete");
                     }, errCallback)
